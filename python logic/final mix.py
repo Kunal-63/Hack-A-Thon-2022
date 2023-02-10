33914c0bf98550d341b8c6b36a86e0a1912a1c49
Kunal Adwani <101789307+Kunal-63@users.noreply.github.com>
2023-02-10 12:57:24 +0530

    mix1

--- python logic/final mix.py
+++ python logic/final mix.py
@@ -1,10 +1,8 @@
 import instaloader
 from datetime import datetime
 from instagrapi import Client
-
 L = instaloader.Instaloader()
 profile = instaloader.Profile.from_username(L.context, "_kunaladwani_")
-
 posts = profile.get_posts()
 try:
     post1 = next(posts)
@@ -15,56 +13,114 @@ try:
     print("Duration between two posts:", duration)
 except:
     pass 
-
-
-L.login("prathamchellani4", "#prathamchellani4")
-
-# print("list of followers")
+L.login("imop690", "#imop690")
 followers = profile.get_followers()
 followers_list = []
 for follower in followers:
     followers_list.append(follower.username)
-    
-# print(" list of followings")
 following_list = []
 followers = profile.get_followees()
 for follower in followers:
-    # print(follower.username)
     following_list.append(follower.username)
-
-print(followers_list)
-print(following_list)
-
 for i in range(followers_list):
     if ( followers_list[i] in following_list ):
         del followers_list[i]
-
 cl = Client()
-cl.login("prathamchellani4", '#prathamchellani4')
+cl.login("imop690", '#imop690')
+
+
+
+#====================================================================================================================
+#====================================================================================================================
+#====================================================================================================================
+
+
+
+real_followers = []
 for i in followers_list:
-    try:
-        print(i)
-        dict=cl.user_info_by_username(i).dict()
-        print("verified",dict["is_verified"])
-        print("private",dict["is_private"])
-        print("follower count",dict["follower_count"])
-        print("following count",dict["following_count"])
-        print("media count",dict["media_count"])
-        print("biography ",dict["biography"])
-        print("\n\n\n")
-    except:
-        pass
+    dict=cl.user_info_by_username(i).dict()
+    if (dict["is_verified"] == True):
+        real_followers.append(i)
+    else:
+        try:
+            profile = instaloader.Profile.from_username(L.context, i)
+            posts = profile.get_posts()
+            post1 = next(posts)
+            post2 = next(posts)
+            timestamp1 = datetime.fromtimestamp(post1.date_local.timestamp())
+            timestamp2 = datetime.fromtimestamp(post2.date_local.timestamp())
+            duration = timestamp2 - timestamp1
+            print(abs(duration.days)," days")
+        except:
+            pass 
+        try:
+
+
+            if dict["is_private"] == False:
+                pass
+            else:
+                pass
+
+
+            follower_following_ratio = int(dict['following_count']) / int(dict['follower_count'])
+            if (follower_following_ratio < 2):
+                pass
+
+
+            if int(dict["media_count"] < 15):
+                pass
+
+
+            if (dict["biography"] == None):
+                    pass
+            else:
+                pass
+
+            print("\n\n\n")
+        except:
+            pass
+    break
 
 for j in following_list:
-    try:
-        print(j)
-        dict=cl.user_info_by_username(j).dict()
-        print("verified",dict["is_verified"])
-        print("private",dict["is_private"])
-        print("follower count",dict["follower_count"])
-        print("following count",dict["following_count"])
-        print("media count",dict["media_count"])
-        print("biography ",dict["biography"])
-        print("\n\n\n")
-    except:
-        pass
+    dict=cl.user_info_by_username(j).dict()
+    if (dict["is_verified"] == True):
+        real_followers.append(j)
+    else:
+        try:
+            profile = instaloader.Profile.from_username(L.context, j)
+            posts = profile.get_posts()
+            post1 = next(posts)
+            post2 = next(posts)
+            timestamp1 = datetime.fromtimestamp(post1.date_local.timestamp())
+            timestamp2 = datetime.fromtimestamp(post2.date_local.timestamp())
+            duration = timestamp2 - timestamp1
+            print(abs(duration.days)," days")
+        except:
+            pass 
+        try:
+
+
+            if dict["is_private"] == False:
+                pass
+            else:
+                pass
+
+
+            follower_following_ratio = int(dict['following_count']) / int(dict['follower_count'])
+            if (follower_following_ratio < 2):
+                pass
+
+
+            if int(dict["media_count"] < 15):
+                pass
+
+
+            if (dict["biography"] == None):
+                    pass
+            else:
+                pass
+
+            print("\n\n\n")
+        except:
+            pass
+    break

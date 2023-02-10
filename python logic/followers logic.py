e94cd095fae9ba07198bf910e2ce52aea0342512
Kunal Adwani <101789307+Kunal-63@users.noreply.github.com>
2023-02-10 12:33:11 +0530

    followers

--- python logic/followers logic.py
+++ python logic/followers logic.py
@@ -3,17 +3,8 @@ from datetime import datetime
 from instagrapi import Client
 
 L = instaloader.Instaloader()
-profile = instaloader.Profile.from_username(L.context, "pratt.css")
-
-# try:
-# posts = profile.get_posts()
-# post1 = next(posts)
-# post2 = next(posts)
-# timestamp1 = datetime.fromtimestamp(post1.date_local.timestamp())
-# timestamp2 = datetime.fromtimestamp(post2.date_local.timestamp())
-# duration = timestamp2 - timestamp1
-# print(abs(duration.days))
-
+profile = instaloader.Profile.from_username(L.context, "fakelightning1")
+real = 0
 
 L.login("first_102030", "first_ig123")
 
@@ -33,7 +24,7 @@ for i in followers_list:
         print(real_followers)
     else:
         try:
-            profile = instaloader.Profile.from_username(L.context, "first_102030")
+            profile = instaloader.Profile.from_username(L.context, i)
             posts = profile.get_posts()
             post1 = next(posts)
             post2 = next(posts)
@@ -41,14 +32,30 @@ for i in followers_list:
             timestamp2 = datetime.fromtimestamp(post2.date_local.timestamp())
             duration = timestamp2 - timestamp1
             print(abs(duration.days)," days")
+            if(abs(duration.days) >= 2):
+                real += 1
         except:
             pass 
         try:
-            print("private",dict["is_private"])
-            print("follower count",dict["follower_count"])
-            print("following count",dict["following_count"])
-            print("media count",dict["media_count"])
-            print("biography ",dict["biography"])
+            # print("private",dict["is_private"])
+            if dict["is_private"] == False:
+                pass
+            else:
+                real += 1
+            follower_following_ratio = int(dict['following_count']) / int(dict['follower_count'])
+            if (follower_following_ratio < 2):
+                real += 1
+            # print("follower count",dict["follower_count"])
+            # print("following count",dict["following_count"])
+            if int(dict["media_count"] < 15):
+                real += 1
+            # print("media count",dict["media_count"])
+            if (dict["biography"] == None):
+                    pass
+            else:
+                real +=1
+            # print("biography ",dict["biography"])
+            print(real/5)
             print("\n\n\n")
         except:
             pass
